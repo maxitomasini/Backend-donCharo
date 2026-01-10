199d7b10c8400b40171743ac4d77c1271ff26e4a
unknown <hola.com>
2026-01-09 21:27:05 -0300

    db nueva

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://don_charo_ws3b_user:elKbKQYzVviJCemp8bqIAxS3AsNzQKWH@dpg-d52nubvpm1nc73esg8vg-a/don_charo_ws3b")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://don_charo_v2zf_user:olGmLIxhaxsNzBYc7mq8g623OYZsh8Go@dpg-d5gpmln5r7bs73avidi0-a/don_charo_v2zf")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
